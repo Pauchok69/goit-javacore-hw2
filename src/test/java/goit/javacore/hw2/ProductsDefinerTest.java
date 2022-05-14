47506cff712a1640711af4d4a7dec4e8b6cf4ade
andy <pauchokbk@gmail.com>
2022-05-14 06:55:19 +0300

    Add test for ProductsDefiner

--- src/test/java/goit/javacore/hw2/ProductsDefinerTest.java
+++ src/test/java/goit/javacore/hw2/ProductsDefinerTest.java
@@ -1,15 +1,18 @@
 package goit.javacore.hw2;
 
-import goit.javacore.hw2.dto.ProductDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 class ProductsDefinerTest {
     @Test
-    void testIfDefineProductListFromCartIsCorrect() {
+    void testDefineProductListFromCartWithExistProducts() {
         ProductsDefiner productsDefiner = new ProductsDefiner();
-        Assertions.assertEquals(7, productsDefiner.defineProductsListFrom("AAABACDWW").size());
+        Assertions.assertEquals(7, productsDefiner.defineProductsListFrom("AAABACD").size());
+    }
+
+    @Test
+    void testDefineProductListFromCartWithNotProductsCharactersInCart() {
+        ProductsDefiner productsDefiner = new ProductsDefiner();
+        Assertions.assertEquals(1, productsDefiner.defineProductsListFrom("   __ 324 A W").size());
     }
 }
