aae582f066ec81c44cb4e810ff2b91f45d9835dd
andy <pauchokbk@gmail.com>
2022-06-24 14:22:44 +0300

    Rewrite ProductsDefinerTest using mockito

--- src/test/java/goit/javacore/hw2/ProductsDefinerTest.java
+++ src/test/java/goit/javacore/hw2/ProductsDefinerTest.java
@@ -1,19 +1,61 @@
 package goit.javacore.hw2;
 
+import goit.javacore.hw2.dto.ProductDTO;
+import goit.javacore.hw2.repository.ProductsRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import static org.mockito.ArgumentMatchers.anyChar;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ProductsDefinerTest {
     //here you have to mock all calls to ProductsRepository
+    @Mock
+    private ProductsRepository productsRepositoryMock;
+    @Mock
+    private ProductDTO productDTOMock;
+    @InjectMocks
+    private ProductsDefiner productsDefiner;
+
+    @BeforeEach
+    void beforeEach() throws NoSuchFieldException, IllegalAccessException {
+        Field field = ProductsDefiner.class.getDeclaredField("productsRepository");
+        field.setAccessible(true);
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        field.set(productsDefiner, productsRepositoryMock);
+
+        when(productsRepositoryMock.findOneByCode(anyChar())).thenReturn(null);
+    }
+
     @Test
     void testDefineProductListFromCartWithExistProducts() {
-        ProductsDefiner productsDefiner = new ProductsDefiner();
+        when(productsRepositoryMock.findOneByCode('A')).thenReturn(productDTOMock);
+        when(productsRepositoryMock.findOneByCode('B')).thenReturn(productDTOMock);
+        when(productsRepositoryMock.findOneByCode('C')).thenReturn(productDTOMock);
+        when(productsRepositoryMock.findOneByCode('D')).thenReturn(productDTOMock);
+
         Assertions.assertEquals(7, productsDefiner.defineProductsListFrom("AAABACD").size());
+        verify(productsRepositoryMock, times(7)).findOneByCode(anyChar());
     }
 
     @Test
     void testDefineProductListFromCartWithNotProductsCharactersInCart() {
-        ProductsDefiner productsDefiner = new ProductsDefiner();
+        when(productsRepositoryMock.findOneByCode('A')).thenReturn(productDTOMock);
+
         Assertions.assertEquals(1, productsDefiner.defineProductsListFrom("   __ 324 A W").size());
+        verify(productsRepositoryMock, times(13)).findOneByCode(anyChar());
     }
 }
